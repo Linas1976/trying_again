13931e67dab4063df3ae99e8f39c1cd523a682ab
Linas1976 <linas.orantas@gmail.com>
2023-07-17 21:37:47 +0000

    h
    Signed-off-by: Linas1976 <linas.orantas@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include "mbed.h"
-printf("hello world");
+
+int main()
+{
+
+// prints hello world
+printf("Hello World");
+}
 InterruptIn bttn(PC_13);
 
 void bttn_interrupt(){
     printf("bttn pressed \r\n");
 }
 
-int main()
+int main2()
 {
     bttn.fall(&bttn_interrupt);
     while (true) {
